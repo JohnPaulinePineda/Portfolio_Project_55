00afded5effe337c14f3d7d43920eb7aaadd0e9d
JohnPaulinePineda <JohnPauline_Pineda@Yahoo.com>
2024-10-06 21:33:34 +0800

    Formulated the Streamlit App
    Updated the streamlit_community_cloud_app code

--- uis/.ipynb_checkpoints/streamlit_community_cloud_app-checkpoint.py
+++ uis/.ipynb_checkpoints/streamlit_community_cloud_app-checkpoint.py
@@ -101,12 +101,14 @@ st.markdown("""---""")
 # and radio buttons for categorical features
 # and storing the user inputs
 ##################################
-input_column_1,input_column_2, input_column_3 = st.columns(3)
-with input_column_2:
+input_column_1, input_column_2, input_column_3, input_column_4, input_column_5, input_column_6 = st.columns(6)
+with input_column_1:
     age_numeric_input = st.slider(variables[0], min_value=20, max_value=100, value=20)
     ejection_fraction_numeric_input = st.slider(variables[1], min_value=10, max_value=80, value=10)
+with input_column_3:
     serum_creatinine_numeric_input = st.slider(variables[2], min_value=0.5, max_value=10.0, value=0.5)
     serum_sodium_numeric_input = st.slider(variables[3], min_value=110, max_value=150, value=50)
+with input_column_5:
     anaemia_categorical_input = st.radio(variables[4], ('Present', 'Absent'), horizontal=True)
     high_blood_pressure_categorical_input = st.radio(variables[5], ('Present', 'Absent'), horizontal=True)
     anaemia_numeric_input = 1 if anaemia_categorical_input == 'Present' else 0
